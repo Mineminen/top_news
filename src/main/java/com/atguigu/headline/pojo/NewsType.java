65a5e48247916e2c92d30dac1eb7efd8098c63c8
ZhangQiuYu <qiuy8685@gmail.com>
2024-05-10 18:51:31 +0800

    提交

---
+++ src/main/java/com/atguigu/headline/pojo/NewsType.java
@@ -0,0 +1,15 @@
+package com.atguigu.headline.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class NewsType implements Serializable {
+    private Integer tid;
+    private String tname;
+}
